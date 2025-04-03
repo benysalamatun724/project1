8c81a419048e188e14b910c5f45bda9de434df37
benysalamatun724 <benysalamatun724@gmail.com>
2025-04-03 14:11:29 +0000

    Add 24.ts

---
+++ src/24.ts
@@ -0,0 +1,16 @@
+function fibonacci(n: number): number {
+  if (n <= 0) return 0;
+  if (n === 1 || n === 2) return 1;
+
+  let a = 0, b = 1, c = 1;
+
+  for (let i = 3; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return c;
+}
+
+console.log(fibonacci(10));
